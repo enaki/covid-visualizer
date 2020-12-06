5390f684518ac5ba376b019d837f0cc2805aafa3
sgabriel190 <strilciuc_gabriel98@yahoo.ro>
2020-12-06 19:47:03 +0200

    Fixed localhost address.

--- apps/CovidVisualizer/app/services/ColorService.js
+++ apps/CovidVisualizer/app/services/ColorService.js
@@ -1,24 +1,24 @@
-var Rainbow = require('rainbowvis.js');
+let Rainbow = require('rainbowvis.js');
 const { min } = require('react-native-reanimated');
-var myRainbow = new Rainbow();
+let myRainbow = new Rainbow();
 myRainbow.setNumberRange(0, 100);
 myRainbow.setSpectrum("#6aff14", "#ffdc14", "#ff8e14", "#ff1414");
 
-const ConnectorService = require('./ConnectorService')
+const ConnectorService = require('./ConnectorService');
 
-var active_per_million = undefined
+let active_per_million = undefined;
 
 const getMoviesFromApiAsync = async () => {
     try {
         active_per_million = await ConnectorService.getCountriesActivePerMillion();
-        console.log(active_per_million);
+        console.log("[INFO-ColorService]: active_per_million=" + active_per_million);
         return active_per_million;
     } catch (error) {
-        console.error(error);
+        console.error("[ERROR-ColorService]: " + error);
     }
 };
-getMoviesFromApiAsync();
-console.log(active_per_million);
+getMoviesFromApiAsync().then(r => {});
+console.log("[INFO-ColorService]: active_per_million=" + active_per_million);
 
 const randomColor = () => {
     let letters = '0123456789ABCDEF';
--- apps/PythonBackendApi/app.py
+++ apps/PythonBackendApi/app.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     database_path = "covid.db"
     drop_script_path = "sql_scripts/drop_covid.db.sql"
     create_script_path = "sql_scripts/covid.db.sql"
-    # init_database(database_path, drop_script_path, create_script_path)
-    # initialize_database_workers(database_path)
+    init_database(database_path, drop_script_path, create_script_path)
+    initialize_database_workers(database_path)
 
     app.run(debug=True, port=2020, ssl_context='adhoc')
 
