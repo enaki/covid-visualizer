4eb366c533a01be3d99b30518dbf6cbea467d007
enaki <enachi98@gmail.com>
2021-01-06 18:09:14 +0200

    Added backend world latest data route by coordinates. Added connector for news.

--- apps/CovidVisualizer/app/screens/NewsScreen.js
+++ apps/CovidVisualizer/app/screens/NewsScreen.js
@@ -1,18 +1,91 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, ActivityIndicator, View, StyleSheet, Linking, TouchableNativeFeedback } from 'react-native'
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import colors from '../config/colors'
+import AsyncStorage from '@react-native-community/async-storage';
+
+import ConnectorService from "../services/ConnectorService";
+import moment from 'moment';
+import { SafeAreaView } from 'react-native';
 
 
-const NewsScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={{ fontSize: 30 }}>News Screen</Text>
-            <Text style={{ fontSize: 20 }}>
-                <Text style={{ fontWeight: 'bold' }}>Today News: </Text>
-                News
-            </Text>
-        </View>
-    );
+class NewsScreen extends React.Component {
+    constructor(props) {
+        console.log("\n[NewsScreen] - Constructor");
+        super(props);
+        this.state = {
+            loadingData: true,
+            articles: []
+        };
+    }
+
+    async componentDidMount() {
+        console.log("[NewsScreen] - componentDidMount");
+        let data;
+        let needUpdate = true;
+        try {
+            if (needUpdate) {
+                data = await ConnectorService.getRomaniaCovidNews();
+                console.log(data);
+            } else {
+                data = localData["data"];
+            }
+            this.setState({ loadingData: false, articles: data });
+        } catch (err) {
+            throw err;
+        }
+
+        console.log("[NewsScreen] - Data Loaded");
+    }
+
+    render() {
+        /*
+        if (!this.state.loadingData) {
+            console.log(this.articles);
+            publishedAt = moment.now();
+            const time = moment(publishedAt || moment.now()).fromNow();
+            const defaultImg = 'https://wallpaper.wiki/wp-content/uploads/2017/04/wallpaper.wiki-Images-HD-Diamond-Pattern-PIC-WPB009691.jpg';
+        }
+*/
+        const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+        return (
+            <SafeAreaView>
+                {
+                    this.state.loadingData ?
+                        <ActivityIndicator
+                            size="large"
+                            color="#bc2b78"
+                            style={styles.activityIndicator}
+                        /> :
+
+                        <TouchableNativeFeedback
+                            useForeground
+                            onPress={() => Linking.openURL(url)}
+                        >
+
+                            <Card>
+                                <Card.Title title="Card Title" subtitle="Card Subtitle" />
+                                <Card.Content>
+                                    <Title>Card title</Title>
+                                    <Paragraph>Card content</Paragraph>
+                                </Card.Content>
+                                <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+
+                            </Card>
+
+                        </TouchableNativeFeedback>
+                }
+
+            </SafeAreaView>
+            /*<View style={styles.container}>
+                <Text style={{ fontSize: 30 }}>News Screen</Text>
+                <Text style={{ fontSize: 20 }}>
+                    <Text style={{ fontWeight: 'bold' }}>Today News: </Text>
+                    News
+                </Text>
+            </View>*/
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -22,6 +95,18 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    noteStyle: {
+        margin: 5,
+        fontStyle: 'italic',
+        color: '#b2bec3',
+        fontSize: 10
+    },
+    featuredTitleStyle: {
+        marginHorizontal: 5,
+        textShadowColor: '#00000f',
+        textShadowOffset: { width: 3, height: 3 },
+        textShadowRadius: 3
+    }
 });
 
 
--- apps/CovidVisualizer/app/services/ConnectorService.js
+++ apps/CovidVisualizer/app/services/ConnectorService.js
@@ -1,4 +1,4 @@
-class ConnectorService{
+class ConnectorService {
     constructor(props) {
         this.requestOptions = {
             method: 'GET',
@@ -9,8 +9,12 @@ class ConnectorService{
         }
         this.IP = "10.0.2.2";
         this.HOST = "2020";
+        this.NEWS_API_KEY = "ceb5a8887632471d959280cb3f31fd55"
+        this.RO_NEWS = `https://newsapi.org/v2/everything?q=covid&sortBy=publishedAt&apiKey=${this.NEWS_API_KEY}&pageSize=100&page=1&language=ro`;
+        this.NEWS = `https://newsapi.org/v2/everything?q=covid&sortBy=publishedAt&apiKey=${this.NEWS_API_KEY}&pageSize=100&page=1&language=en&sources=bbc-news,nbc-news,new-scientist`;
+
     }
-    getCountriesActivePerMillion(){
+    getCountriesActivePerMillion() {
         return fetch(`http://${this.IP}:${this.HOST}/api/maps/countries`, this.requestOptions)
             .then((response) => response.json())
             .then((json) => {
@@ -20,7 +24,7 @@ class ConnectorService{
                 console.error(error);
             });
     }
-    getRoCountiesActivePerOneHundred(){
+    getRoCountiesActivePerOneHundred() {
         return fetch(`http://${this.IP}:${this.HOST}/api/maps/regions/ro`, this.requestOptions)
             .then((response) => response.json())
             .then((json) => {
@@ -42,6 +46,36 @@ class ConnectorService{
     }
     getWorldLatestData() {
         return fetch(`http://${this.IP}:${this.HOST}/api/latest/world`, this.requestOptions)
+            .then((response) => response.json())
+            .then((json) => {
+                return json;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+    getCountryLatestDataByLatAndLong(latitude, longitude) {
+        return fetch(`http://${this.IP}:${this.HOST}/api/latest/countries/coordinates?latitude=${latitude}&longitude=${longitude}`, this.requestOptions)
+            .then((response) => response.json())
+            .then((json) => {
+                return json;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+    getWorldCovidNews() {
+        return fetch(`${this.NEWS}`, this.requestOptions)
+            .then((response) => response.json())
+            .then((json) => {
+                return json;
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+    getRomaniaCovidNews() {
+        return fetch(`${this.RO_NEWS}`, this.requestOptions)
             .then((response) => response.json())
             .then((json) => {
                 return json;
